f23abb9a9d42cfce59b75ace7aaf40b48db956f9
Isaac Mann <isaacplmann@gmail.com>
2023-02-17 11:34:16 -0500

    in progress generator

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -9,61 +9,15 @@ import {
 } from '@nrwl/devkit';
 import * as path from 'path';
 import { UtilLibGeneratorSchema } from './schema';
-
-interface NormalizedSchema extends UtilLibGeneratorSchema {
-  projectName: string;
-  projectRoot: string;
-  projectDirectory: string;
-  parsedTags: string[];
-}
-
-function normalizeOptions(tree: Tree, options: UtilLibGeneratorSchema): NormalizedSchema {
-  const name = names(options.name).fileName;
-  const projectDirectory = options.directory
-    ? `${names(options.directory).fileName}/${name}`
-    : name;
-  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
-  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
-  const parsedTags = options.tags
-    ? options.tags.split(',').map((s) => s.trim())
-    : [];
-
-  return {
-    ...options,
-    projectName,
-    projectRoot,
-    projectDirectory,
-    parsedTags,
-  };
-}
-
-function addFiles(tree: Tree, options: NormalizedSchema) {
-    const templateOptions = {
-      ...options,
-      ...names(options.name),
-      offsetFromRoot: offsetFromRoot(options.projectRoot),
-      template: ''
-    };
-    generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
-}
+import { libraryGenerator } from '@nrwl/js';
 
 export default async function (tree: Tree, options: UtilLibGeneratorSchema) {
-  const normalizedOptions = normalizeOptions(tree, options);
-  addProjectConfiguration(
-    tree,
-    normalizedOptions.projectName,
-    {
-      root: normalizedOptions.projectRoot,
-      projectType: 'library',
-      sourceRoot: `${normalizedOptions.projectRoot}/src`,
-      targets: {
-        build: {
-          executor: "@bg-hoard/internal-plugin:build",
-        },
-      },
-      tags: normalizedOptions.parsedTags,
-    }
-  );
-  addFiles(tree, normalizedOptions);
+  options.name = 'util-' + options.name;
+  if (options.tags) {
+    options.tags += `,type:util,scope:${options.directory}`;
+  } else {
+    options.tags = `type:util,scope:${options.directory}`;
+  }
+  await libraryGenerator(tree, options);
   await formatFiles(tree);
 }
--- libs/internal-plugin/src/generators/util-lib/schema.d.ts
+++ libs/internal-plugin/src/generators/util-lib/schema.d.ts
@@ -1,5 +1,5 @@
 export interface UtilLibGeneratorSchema {
     name: string;
     tags?: string;
-    directory?: string;
+    directory?: 'store' | 'api' | 'shared';
 }
