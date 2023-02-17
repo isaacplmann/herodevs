8e7a9905327f2f1a52b85c3a8fe36f540f2c34cc
Isaac Mann <isaacplmann@gmail.com>
2023-02-17 13:39:55 -0500

    fixes

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -1,16 +1,12 @@
-import {
-  formatFiles, Tree
-} from '@nrwl/devkit';
+import { formatFiles, Tree } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/js';
 import { UtilLibGeneratorSchema } from './schema';
 
 export default async function (tree: Tree, options: UtilLibGeneratorSchema) {
   options.name = 'util-' + options.name;
-  if (options.tags) {
-    options.tags += `,type:util,scope:${options.directory}`;
-  } else {
-    options.tags = `type:util,scope:${options.directory}`;
-  }
-  await libraryGenerator(tree, options);
+  await libraryGenerator(tree, {
+    ...options,
+    tags: `type:util,scope:${options.directory}`,
+  });
   await formatFiles(tree);
 }
